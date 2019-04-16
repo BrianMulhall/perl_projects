2c82596dde87b5fda7fdad5e8b7bfe5fbeef1e40
BrianMulhall <brian.e.mulhall@gmail.com>
2019-04-16 14:50:06 -0500

    playing around in the sandbox file a bit and added in an exception example as well

--- sandbox.pl
+++ sandbox.pl
@@ -1,5 +1,13 @@
-use Mojolicious::Lite;
+use strict;
+use warnings;
+use v5.28.1;
 
-get '/' => {text => 'I ♥ Mojolicious!'};
+my @arr = (1,2,3,'brian','isabelle',5.50,10.001);
 
-app->start;
+# foreach my $a (@arr){
+# 	print $a."\n";
+# }
+
+
+say $arr[3];
+say $arr[4];
