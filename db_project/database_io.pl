dec2a04cd4196cb3621c0415b8e73fe7aeab15cd
BrianMulhall <brian.e.mulhall@gmail.com>
2019-04-16 09:16:22 -0500

    continueing to add files for the books i am reading

--- db_project/database_io.pl
+++ db_project/database_io.pl
@@ -3,29 +3,43 @@ use warnings;
 use DBI;
 use v5.28.1;
 
-my @drivers = DBI->available_drivers;
-print join(", ", @drivers), "\n";
+#!/usr/bin/perl -w # # ch04/listdsns: Enumerates all data sources and all installed drivers # use DBI; 
+ 
+# ### Probe DBI for the installed drivers 
+# my @drivers = DBI->available_drivers(); 
 
-my @dsns = DBI->data_sources('ODBC');
-foreach my $d (@dsns)
-{
-  print "$d\n";
-}
+# die "No drivers found!\n" unless @drivers; # should never happen 
+ 
+# ### Iterate through the drivers and list the data sources for each one 
+# foreach my $driver ( @drivers ) {     
+# 	print "Driver: $driver\n";     
+# 	my @dataSources = DBI->data_sources( $driver );     
+# 	foreach my $dataSource ( @dataSources ) {         
+# 		print "\tData Source is $dataSource\n";    
+# 	}     
+# 	print "\n"; } 
+ 
+# create the data source that we are connecting to
+my $dsn = qq(dbi:ODBC:desktop_db);
 
+# make the object you interact with the db with
+my $dbh = DBI->connect($dsn,{ RaiseError => 1}) or die DBI->errstr;
 
-my $dbh = DBI->connect("DBI:ODBC:$myDSN",{ RaiseError => 1}) or die DBI->errstr;
+my $sqlStatement = qq(select * from [dbo].[Person]);      # the query to execute
 
-my $sql = qq(select lastname from dbo.Resources where lastname is not null AND lastname != "" );      # the query to execute
-my $sth = $dbh->prepare($sql);          # prepare the query
+my $sth = $dbh->prepare($sqlStatement);          # prepare the query
 $sth->execute();   
 
 
 my $i = 0;                     # execute the query
 my @row;
 while (@row = $sth->fetchrow_array) {  # retrieve one row
-    print $row[0] . "\n";
+    say "@row"	;
 	$i++;
 }
 
 print "Number of records is equal to : " . $i;
-$dbh->disconnect();
+### Now, disconnect from the database 
+$dbh->disconnect or warn "Disconnection failed: $DBI::errstr\n"; 
+
+
